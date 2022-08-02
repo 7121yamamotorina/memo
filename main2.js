eda95f1257c53ebc4b42ba65cb4c4fecfae3dbb1
yamamotorina <b11u0155@gmail.com>
2022-08-02 04:36:58 +0000

    keisanki

--- main2.js
+++ main2.js
@@ -7,7 +7,7 @@ $(".dan").on("click", function() {
   }
   else if (target_value == "=") {
     result.value = eval(result.value);
-  }
+  } 
   else {
     if (result.value == "0") {
       result.value = target_value;
